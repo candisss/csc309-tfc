db3709fc2393a1f10cbb18920461791077859350
Angela <angela.wei@mail.utoronto.ca>
2022-11-09 10:37:57 -0500

    added custom user

--- tfc/accounts/models.py
+++ tfc/accounts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.model import AbstractUser
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    subscribed = models.BooleanField()
+    avatar = models.ImageField(required=False)
