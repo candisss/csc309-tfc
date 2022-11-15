6e593e6331eadab8b68ba38f80d8dde46a1291d9
ranhaomi <haoming.ran@mail.utoronto.ca>
2022-11-14 20:52:05 -0500

    Update subscription - Models

--- tfc/subscriptions/admin.py
+++ tfc/subscriptions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from subscriptions.models import PaymentCards, PaymentHistory, Subscriptions
+
 # Register your models here.
+
+admin.site.register(Subscriptions)
+admin.site.register(PaymentCards)
+admin.site.register(PaymentHistory)
--- tfc/subscriptions/models.py
+++ tfc/subscriptions/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models import CASCADE
+
 
 # Create your models here.
+class Subscriptions(models.Model):
+    price = models.CharField(max_length=50)
+    # term can only be "year" or "month"
+    term = models.CharField(max_length=50)
+
+class PaymentCards(models.Model):
+    card_holder_name = models.CharField(max_length=100)
+    card_num = models.CharField(max_length=50)
+    expiry_date = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=50)
+    billing_address = models.CharField(max_length=100)
+
+class PaymentHistory(models.Model):
+    amount_paid = models.CharField(max_length=50)
+    payment_card = models.CharField(max_length=50)
+    date = models.DateField()
+    time = models.TimeField()
+
+
