0b1d0f36b48b78846f46ba3bd20f410e14dac76e
jenny16888 <76978696+jenny16888@users.noreply.github.com>
2022-11-20 05:06:08 -0500

    update accounts admin

--- tfc/accounts/admin.py
+++ tfc/accounts/admin.py
@@ -6,9 +6,9 @@ from accounts.models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'is_subscribed')
+    list_display = ('username', 'first_name', 'last_name')
 
     fieldsets = (
-        ('Basic', {'fields': ('first_name', 'last_name', 'username', 'email', 'phone_num', 'avatar', 'is_subscribed', 'subscription', 'is_admin', 'is_staff',
+        ('Basic', {'fields': ('first_name', 'last_name', 'username', 'email', 'phone_num', 'avatar', 'subscription', 'is_admin', 'is_staff',
                              'is_superuser')}),
     )
