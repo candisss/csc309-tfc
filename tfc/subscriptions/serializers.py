a89803dcafc12e29ede58db790505d21b77f320d
ranhaomi <haoming.ran@mail.utoronto.ca>
2022-11-20 21:29:14 -0500

    update subscription views

--- tfc/subscriptions/serializers.py
+++ tfc/subscriptions/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from rest_framework.fields import CurrentUserDefault
+import re
 
 from subscriptions.models import Subscription, PaymentCard, PaymentHistory
 
@@ -10,18 +11,31 @@ class SubscriptionSerializer(serializers.ModelSerializer):
         fields = ['price', 'term']
 
 
+class PaymentHistorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PaymentHistory
+        fields = ['amount_paid', 'payment_card', 'datetime', 'user']
+
+
 class PaymentCardSerializer(serializers.ModelSerializer):
-    user = CurrentUserDefault()
 
     class Meta:
         model = PaymentCard
         fields = ['card_holder_name', 'card_num', 'expiry_date', 'postal_code',
                   'billing_address', 'user']
+        read_only_fields = ['user']
 
+    def validate_card_num(self, data):
+        card_num = data
+        num_regex = re.compile('\d{16}')
+        if not num_regex.match(card_num):
+            raise serializers.ValidationError('Enter a valid card number.')
+        return card_num
 
-class PaymentHistorySerializer(serializers.ModelSerializer):
-    user = CurrentUserDefault()
-
-    class Meta:
-        model = PaymentHistory
-        fields = ['amount_paid', 'payment_card', 'datetime', 'user']
+    def validate_expiry_date(self, data):
+        expiry_date = data
+        date_regex = re.compile('^(0[1-9]|1[0-2])\/?([0-9]{2})$')
+        if not date_regex.match(expiry_date):
+            raise serializers.ValidationError('Enter a valid expiry date.')
+        return expiry_date
--- tfc/tfc/settings.py
+++ tfc/tfc/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,4 +139,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ]
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=5)
 }
